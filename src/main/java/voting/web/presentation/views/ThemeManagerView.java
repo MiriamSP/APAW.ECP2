52d073fec3daeb6d590319c243f6e7a8d2eac0f6
msanzpascua <msanzpascua@aeport1067.acciona.int>
2015-10-22 20:14:35 +0200

    fin ThemeManagerView

--- src/main/java/voting/web/presentation/views/ThemeManagerView.java
+++ src/main/java/voting/web/presentation/views/ThemeManagerView.java
@@ -1,27 +1,20 @@
 package voting.web.presentation.views;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import voting.rest.business.controllers.ThemeBusinessController;
-import voting.rest.business.models.entities.Theme;
-import voting.rest.business.views.ThemeTransfer;
 import voting.web.presentation.models.Model;
 
 public class ThemeManagerView implements View {
     @Override
     public void show(Model model) {
         System.out.println("Theme Manager Page\n");
-
-        //String s = (String) model.get("themeName");
-        //System.out.println(s + "\n");
-
         List<String> themesList = (List<String>) model.get("themeName");
-        System.out.print("Temas: [ ");
+        System.out.print("Temas: [");
         for (int i = 0; i < themesList.size(); i++) {
-            System.out.print(themesList.get(i) + " ");
+            System.out.print(themesList.get(i));
+            if (i < themesList.size() - 1) {
+                System.out.print(", ");
+            }
         }
         System.out.print("] \n");
-
     }
 }
