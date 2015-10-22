50ce26754c5da1b5f61e991603c05ceef1ea818f
msanzpascua <msanzpascua@aeport1067.acciona.int>
2015-10-22 23:17:05 +0200

    commit view formateado

--- src/main/java/voting/web/presentation/views/VotingView.java
+++ src/main/java/voting/web/presentation/views/VotingView.java
@@ -1,21 +1,15 @@
 package voting.web.presentation.views;
 
 import java.util.List;
-
-import voting.rest.business.models.entities.Vote;
 import voting.web.presentation.models.Model;
 
 public class VotingView implements View {
 
     @Override
-
     public void show(Model model) {
         System.out.println("Voting Page\n");
         List<String> votesList = (List<String>) model.get("themeValueVote");
-        System.out.print("[MSP] votesList.size: " + votesList.size()+ "\n");
-
         System.out.print("Temas: ");
-
         for (int i = 0; i < votesList.size(); i++) {
             System.out.print("[ThemeTransfer " + votesList.get(i) + "]");
 
@@ -23,7 +17,6 @@ public class VotingView implements View {
                 System.out.print(", ");
             }
         }
-
         System.out.print("] \n");
     }
 
