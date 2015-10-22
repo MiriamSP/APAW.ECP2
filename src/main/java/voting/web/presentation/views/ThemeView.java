c3851b8ef80a22ff058239cef924b197c080f489
msanzpascua <msanzpascua@aeport1067.acciona.int>
2015-10-22 11:59:21 +0200

    commit View

--- src/main/java/voting/web/presentation/views/ThemeView.java
+++ src/main/java/voting/web/presentation/views/ThemeView.java
@@ -1,12 +1,19 @@
 package voting.web.presentation.views;
 
+import java.util.List;
+
 import voting.web.presentation.models.Model;
 
 public class ThemeView implements View {
-
     @Override
     public void show(Model model) {
-        System.out.println("ThemeManager \n" + model.get("msg"));
-    }
+        System.out.println("----- ThemeManager ------\n");
 
+        List<String> themesList = (List<String>) model.get("msg");
+        System.out.print("Temas: [ ");
+        for (String themeName : themesList) {
+            System.out.print(themeName + " ");
+        }
+        System.out.print("] \n");
+    }
 }
--- src/main/java/voting/web/presentation/views/VoteView.java
+++ src/main/java/voting/web/presentation/views/VoteView.java
@@ -1,12 +1,19 @@
 package voting.web.presentation.views;
 
+import java.util.List;
+
+import voting.rest.business.models.entities.Vote;
 import voting.web.presentation.models.Model;
 
 public class VoteView implements View {
 
     @Override
+
     public void show(Model model) {
-        System.out.println("Voting \n" + model.get("msg"));
+        System.out.println("----- Voting ------\n");
+        Object vote = model.get("msg");
+
+        System.out.print("] \n");
     }
 
 }
